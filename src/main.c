1c9248a17c46f418fd0923b5019fd5c4cd7b0d73
Hudson C. Dalpra <hudson@bduncanltd.com>
2023-11-14 16:39:10 +1300

    added: anjay mandatory objects

--- src/main.c
+++ src/main.c
@@ -1,5 +1,61 @@
 #include <anjay/anjay.h>
+#include <anjay/security.h>
+#include <anjay/server.h>
 #include <avsystem/commons/avs_log.h>
+
+// Installs Security Object and adds and instance of it.
+// An instance of Security Object provides information needed to connect to
+// LwM2M server.
+static int setup_security_object(anjay_t *anjay)
+{
+	if (anjay_security_object_install(anjay)) {
+		return -1;
+	}
+
+	const anjay_security_instance_t security_instance = {
+		.ssid = 1,
+		.server_uri = "coap://eu.iot.avsystem.cloud:5683",
+		.security_mode = ANJAY_SECURITY_NOSEC};
+
+	// Anjay will assign Instance ID automatically
+	anjay_iid_t security_instance_id = ANJAY_ID_INVALID;
+	if (anjay_security_object_add_instance(anjay, &security_instance, &security_instance_id)) {
+		return -1;
+	}
+
+	return 0;
+}
+
+// Installs Server Object and adds and instance of it.
+// An instance of Server Object provides the data related to a LwM2M Server.
+static int setup_server_object(anjay_t *anjay)
+{
+	if (anjay_server_object_install(anjay)) {
+		return -1;
+	}
+
+	const anjay_server_instance_t server_instance = {
+		// Server Short ID
+		.ssid = 1,
+		// Client will send Update message often than every 60 seconds
+		.lifetime = 60,
+		// Disable Default Minimum Period resource
+		.default_min_period = -1,
+		// Disable Default Maximum Period resource
+		.default_max_period = -1,
+		// Disable Disable Timeout resource
+		.disable_timeout = -1,
+		// Sets preferred transport to UDP
+		.binding = "U"};
+
+	// Anjay will assign Instance ID automatically
+	anjay_iid_t server_instance_id = ANJAY_ID_INVALID;
+	if (anjay_server_object_add_instance(anjay, &server_instance, &server_instance_id)) {
+		return -1;
+	}
+
+	return 0;
+}
 
 int main(int argc, char *argv[])
 {
@@ -19,8 +75,16 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	anjay_event_loop_run(anjay, avs_time_duration_from_scalar(1, AVS_TIME_S));
+	int result = 0;
+	// Setup necessary objects
+	if (setup_security_object(anjay) || setup_server_object(anjay)) {
+		result = -1;
+	}
+
+	if (!result) {
+		result = anjay_event_loop_run(anjay, avs_time_duration_from_scalar(1, AVS_TIME_S));
+	}
 
 	anjay_delete(anjay);
-	return 0;
+	return result;
 }
